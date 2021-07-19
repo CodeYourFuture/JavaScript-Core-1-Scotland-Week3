2f7daae0a3433b9ed156cf8b7d3a2ba0b568ea33
Megumi Yanaka <margaret_mori0102@yahoo.com>
2021-07-19 18:02:46 +0100

    Class/A-Review finishe

--- Class/A-Review/exercise1.js
+++ Class/A-Review/exercise1.js
@@ -4,7 +4,13 @@ let ageOfReshma = 25;
 // Write an "if/else" statement below to print
 // "Reshma is the same age as me" if Reshma is your age
 // "Reshma is not my age" if Reshma is older or younger than you
+let ageOfmine = 36;
 
+if( ageOfReshma === ageOfmine ){
+    console.log("Reshma is the same age as me");
+}else{
+    console.log("Reshma is not my age");
+}
 
 
 
--- Class/A-Review/exercise3.js
+++ Class/A-Review/exercise3.js
@@ -8,9 +8,11 @@ let myName = "Richard";
 if(myName === "Richard"){
     // This variable has LOCAL scope, it only exists inside the "if" statement
     let myAge = 45;
+    console.log(myAge);
 }
 
 // This line of code throws an error! What is the error?
-console.log(myAge);
+//console.log(myAge);
 
 // How can we fix the error?
+//Answer: move "console.log(myAge);" in line 15 into line 11
